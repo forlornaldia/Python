c53d06a2b6e0f46ee7a6e491a7f92139c3960aaa
forlornaldia <vegeta2167@gmail.com>
2022-09-12 12:54:22 -0400

    9/12/2022

---
+++ Week 3/parameters.py
@@ -0,0 +1,13 @@
+char_name = input("What is your name? ")
+char_role = input("What is your character's role? ")
+
+def nameol(de_name, de_role):
+    print(f"Your name is {de_name}.")
+    print(f"Your role is {de_role}.")
+
+nameol(char_name, char_role)
+
+char_name2 = input("What is your name? ")
+char_role2 = input("What is your character's role? ")
+
+nameol(char_name2, char_role2)
